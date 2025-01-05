565fd8985e4fc06d6e9fb1c43a1fbb10e932450c
Christopher Cook <christophercook@unitedparadigms.com>
2025-01-05 03:46:06 -0600

    pelsplsesl

--- .github/scripts/version-tag.js
+++ .github/scripts/version-tag.js
@@ -1,24 +1,23 @@
-// import {readPackage} from "read-pkg"
-// import {parse as parseSemanticVersion} from "semver"
-// 
-// const packageVersion = (
-// 	await readPackage({cwd: "../", normalize: false})
-// ).version
-// const {major, minor, patch, prerelease} = parseSemanticVersion(packageVersion)
-// 
-// let lesserParts
-// if (minor == 0 && patch == 0)
-// 	lesserParts = ""
-// else if (patch == 0)
-// 	lesserParts = `.${minor}`
-// else
-// 	lesserParts = `.${minor}.${patch}`
-// 
-// let prereleasePart
-// if (prerelease.length == 0)
-// 	prereleasePart = ""
-// else
-// 	prereleasePart = `-${prerelease.join(".")}`
-// 
-// export const versionTag = `versions/${major}${lesserParts}${prereleasePart}`
-export const versionTag = `0.0.122`
+import {readPackage} from "read-pkg"
+import {parse as parseSemanticVersion} from "semver"
+
+const packageVersion = (
+	await readPackage({cwd: "./", normalize: false})
+).version
+const {major, minor, patch, prerelease} = parseSemanticVersion(packageVersion)
+
+let lesserParts
+if (minor == 0 && patch == 0)
+	lesserParts = ""
+else if (patch == 0)
+	lesserParts = `.${minor}`
+else
+	lesserParts = `.${minor}.${patch}`
+
+let prereleasePart
+if (prerelease.length == 0)
+	prereleasePart = ""
+else
+	prereleasePart = `-${prerelease.join(".")}`
+
+export const versionTag = `versions/${major}${lesserParts}${prereleasePart}`
